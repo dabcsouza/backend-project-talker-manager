b4d8d0e4d3efdde9474ee11deb0aca476177e33d
David Souza <dabcs@poli.ufrj.br>
2022-02-15 15:15:38 -0300

    alteração

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-// não remova esse endpoint, e para o avaliador funionar
+// não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
